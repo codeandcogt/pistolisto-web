f44033d1ba9498a9e1c0a5673beafa03fa58d8fd
asofimp <asofimp@gmail.com>
2025-10-23 23:27:12 -0600

    marker

--- src/components/atom/marker/marker.tsx
+++ src/components/atom/marker/marker.tsx
@@ -5,7 +5,7 @@ import React from "react"
 import { Marker, Popup, useMapEvents } from "react-leaflet"
 
 const icon = new Icon({
-  iconUrl: '/image/alfiler.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
   iconSize: [30,30],
   iconAnchor: [41, 41]
 })
