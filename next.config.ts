ace5d4833c53caaf9e2fe4a60f2512a102cf694d
asofimp <asofimp@gmail.com>
2025-10-24 19:17:26 -0600

    feat: changes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
-// import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,12 +5,14 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'pistolisto-statics-web.s3.us-east-1.amazonaws.com',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: '*.s3.*.amazonaws.com',
-        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.s3.amazonaws.com',
       },
     ],
   },
